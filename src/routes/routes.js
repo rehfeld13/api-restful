e42becfeef25a8e8ec018ff93576f05f8c3e05b7
Rafael <rafaelrehfeld97@gmail.com>
2023-04-05 12:44:30 -0300

    add method get

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,7 +8,7 @@ const ProductsController = require('../controllers/products')
 //DELETE - REMOVER DADOS
 
 
-router.get('/products', ProductsController.get)
+router.get('/products/:id?', ProductsController.get)
 //router.post('/products', ProductsController.post)
 //router.put('/products/:id', ProductsController.put)
 //router.delete('/products/:id', ProductsController.delete)
